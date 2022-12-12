d1b6043b02f835389a0f04ff9ac5ae0f2efc986d
Divyesh-Sar2003 <119989934+Divyesh-Sar2003@users.noreply.github.com>
2022-12-12 08:46:32 +0530

    java table program
    this is java table program

---
+++ Table.java
@@ -0,0 +1,14 @@
+class Table
+{
+	public static void main(String args[])
+	{
+		System.out.println("Table no is : ");
+		int no=28;
+		for (int i=1;i<=10;i++) 
+		{
+			int t=no*i;
+			System.out.println("\t"+no+"*"+i+"="+t+"\n");
+		}
+
+	}
+}
